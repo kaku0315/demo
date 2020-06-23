f00e248410eb4012a2ffe38bc3d6082661f57424
vic <vic@linktech.com.tw>
2020-06-23 21:14:42 +0800

    DEVD-5

--- app/src/main/java/com/example/linktech/MainActivity.java
+++ app/src/main/java/com/example/linktech/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
+//update java file_0620
 public class MainActivity extends AppCompatActivity {
 
     EditText mTextUsername;
